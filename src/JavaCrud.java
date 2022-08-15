de83a2041e670adc293f514bcaf4e85024deaea2
erick200011 <75103508+erick200011@users.noreply.github.com>
2022-08-15 17:32:25 -0500

    Crud_Login

---
+++ src/JavaCrud.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class JavaCrud {
+    private JPanel Main;
+    private JTextField textNombre;
+    private JTextField textPrecio;
+    private JTextField textCiudad;
+    private JButton createButton;
+    private JButton deleteButton;
+    private JButton updateButton;
+    private JTextField textCantidad;
+    private JButton limpiarButton;
+    private JButton buscarButton;
+    private JTextField textID;
+    private JLabel textMensaje;
+}
---
+++ src/Login.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Login {
+    private JPanel loginPanel;
+    private JButton OKButton;
+    private JButton cancelButton;
+    private JTextField emailTF;
+    private JPasswordField passwordTF;
+}
